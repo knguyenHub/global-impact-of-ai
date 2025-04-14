d9e16d443a349161f5e350ff8cbf593153ae04d9
Aaliyah McCollum <aaliyahlynnette@outlook.com>
2025-04-14 14:19:54 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 import pandas as pd
 import numpy as np
+import matplotlib as mlp 
 from sklearn import preprocessing
+
+# 1. download conda: https://www.anaconda.com/docs/getting-started/miniconda/install#windows-command-prompt
+# 2. after download create environment in terminal:
+#       conda create -n DataScienceProject 
+#   where DataScienceProject is the name of the environment
+# 3. activate the environment:
+#   conda activate DataScienceProject
+# 4. install packages: 
+#   conda install matplotlib scikit-learn pandas
+# 5. Open your command pallet using settings gear in bottom left corner
+# 6. Select Python Interpreter: Python 3.13.2 ('DataScienceProject)
+# 7. Run file to make sure theres no install issues
+
+
+
